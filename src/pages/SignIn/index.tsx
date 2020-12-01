131a93fde2340f5d92cfdc8f2932a8c9619f6cd8
Matheus Lucas Silva Meirelles Reis <matheus.reis@polsec.ai>
2020-12-01 01:23:19 -0300

    finish login & register styles and add context

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import { useAuth } from '../../hooks/AuthContext';
+import getValidationErros from '../../utils/getValidationErrors';
+
 import { Container, Content, Background } from './styles';
 
 import logoImg from '../../assets/logo.svg';
@@ -7,34 +14,70 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+interface SignInaFormData {
+  email: string;
+  password: string;
+}
 
-      <form>
-        <h1>Faça seu logon</h1>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { signIn } = useAuth();
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="senha"
-        />
+  const handleSubmit = useCallback(
+    async (data: SignInaFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-        <Button type="submit">Entrar</Button>
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('Email obrigatório')
+            .email('Digite um email Válido'),
+          password: Yup.string().min(6, 'Senha obrigatória'),
+        });
 
-      <a href="login">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
+        await schema.validate(data, { abortEarly: false });
 
-    <Background />
-  </Container>
-);
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        const errors = getValidationErros(err);
+        formRef.current?.setErrors(errors);
+      }
+    },
+    [signIn],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,39 +1,67 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { abort } from 'process';
 import { Container, Content, Background } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationErros from '../../utils/getValidationErrors';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-      <form>
-        <h1>Faça seu cadastro</h1>
+  const handleSubmit = useCallback(async data => {
+    try {
+      formRef.current?.setErrors({});
 
-        <Input name="name" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="senha"
-        />
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Digite um email Válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
 
-        <Button type="submit">Cadastrar</Button>
-      </form>
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      const errors = getValidationErros(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
-      <a href="login">
-        <FiArrowLeft />
-        Voltar para logon
-      </a>
-    </Content>
-  </Container>
-);
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit} ref={formRef}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
