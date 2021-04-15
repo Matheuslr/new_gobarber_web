4001a856affd4e37b116bfaacc8b7c3fc6fadf33
matheuslr <matheuslr27@gmail.com>
2021-04-15 01:14:15 -0300

    Start dashboard build

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,177 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { FiClock, FiPower } from 'react-icons/fi';
+import { boolean } from 'yup';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Appointment,
+  Section,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const { signOut, user } = useAuth();
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>Matheus Reis</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>dia 6</span>
+            <span>segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars.githubusercontent.com/u/40533045?s=400&u=3d79e8af50482c2d190ab04615e3e4164c77d5c5&v=4"
+                alt="Matheus Reis"
+              />
+
+              <strong>Matheus Reis</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/40533045?s=400&u=3d79e8af50482c2d190ab04615e3e4164c77d5c5&v=4"
+                  alt="Matheus Reis"
+                />
+                <strong>Matheus reis</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/40533045?s=400&u=3d79e8af50482c2d190ab04615e3e4164c77d5c5&v=4"
+                  alt="Matheus Reis"
+                />
+                <strong>Matheus reis</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/40533045?s=400&u=3d79e8af50482c2d190ab04615e3e4164c77d5c5&v=4"
+                  alt="Matheus Reis"
+                />
+                <strong>Matheus reis</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/40533045?s=400&u=3d79e8af50482c2d190ab04615e3e4164c77d5c5&v=4"
+                  alt="Matheus Reis"
+                />
+                <strong>Matheus reis</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Calendar>
+            <DayPicker
+              weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+              fromMonth={new Date()}
+              disabledDays={[
+                {
+                  daysOfWeek: [0, 6],
+                },
+              ]}
+              modifiers={{
+                available: { daysOfWeek: [1, 2, 3, 4, 5] },
+              }}
+              selectedDays={selectedDate}
+              onDayClick={handleDateChange}
+              months={[
+                'Janeiro',
+                'Fevereiro',
+                'Março',
+                'Abril',
+                'Maio',
+                'Junho',
+                'Julho',
+                'Agosto',
+                'Setembro',
+                'Outubro',
+                'Novembro',
+                'Dezembro',
+              ]}
+            />
+          </Calendar>
+        </Schedule>
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
